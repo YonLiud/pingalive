535fc104805bf940a68f4a1eb38c85893371dea4
Yon <ejliud@gmail.com>
2024-12-28 15:11:40 +0200

    feat: Refactor code
    - Support multiple IPs, display each IP's status on its own line
    - Add concurrency for checking connections.
    - Spinner animation now updates independently for each IP.
    - Updated terminal handling for smoother output.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module pingalive
 go 1.23.3
 
 require (
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net"
 	"os"
 	"time"
 
+	"github.com/spf13/pflag"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
+)
+
+const (
+	Reset = "\033[0m"
+	White = "\033[37m"
+	Green = "\033[32m"
+	Red   = "\033[31m"
+	Cyan  = "\033[36m"
+	RedBG = "\033[41m"
 )
 
 func checkConnection(ip, msg string) bool {
@@ -44,19 +53,19 @@ func checkConnection(ip, msg string) bool {
 	return err == nil
 }
 
-func parseFlags() (string, int, string, string) {
-	var ip string
+func parseFlags() ([]string, int, string, string) {
+	var ips []string
 	var interval int
 	var msg string
 	var spinnerStyle string
 
-	flag.StringVar(&ip, "ip", "8.8.8.8", "Custom IP address to ping")
-	flag.IntVar(&interval, "interval", 250, "Custom interval (in ms) between checks")
-	flag.StringVar(&msg, "msg", "ping", "Custom message to send in the ICMP request")
-	flag.StringVar(&spinnerStyle, "spinner", "default", "Choose spinner style: default, or clock")
-	flag.Parse()
+	pflag.StringSliceVar(&ips, "ips", []string{"8.8.8.8"}, "Custom IP addresses to ping (comma-separated)")
+	pflag.IntVar(&interval, "interval", 250, "Custom interval (in ms) between checks")
+	pflag.StringVar(&msg, "msg", "ping", "Custom message to send in the ICMP request")
+	pflag.StringVar(&spinnerStyle, "spinner", "default", "Choose spinner style: default, or clock")
+	pflag.Parse()
 
-	return ip, interval, msg, spinnerStyle
+	return ips, interval, msg, spinnerStyle
 }
 
 // ? Contributors! Add your spinner style here! make it unique and fun!
@@ -70,37 +79,44 @@ func getSpinnerStyle(spinnerStyle string) []string {
 	}
 }
 
-func printStatus(isAlive bool, spinner []string, i int, ip string) {
-	const (
-		Reset = "\033[0m"
-		White = "\033[37m"
-		Green = "\033[32m"
-		Red   = "\033[31m"
-		Cyan  = "\033[36m"
-		RedBG = "\033[41m"
-	)
-
+func printStatus(isAlive bool, spinner []string, i int, ip string, index int) {
 	if isAlive {
-		fmt.Printf("\r%s %sOK%s %s%s.    ", spinner[i], Green, Reset, ip, Reset)
+		fmt.Printf("\033[%d;0H%s %sOK%s %s%s    \n", index+1, spinner[i], Green, Reset, ip, Reset)
 	} else {
-		fmt.Printf("\r%s %s%sDEAD%s %s%s. ", spinner[i], RedBG, White, Reset, ip, Reset)
+		fmt.Printf("\033[%d;0H%s %s%sDEAD%s %s%s \n", index+1, spinner[i], RedBG, White, Reset, ip, Reset)
 	}
 }
 
 func main() {
-	ip, interval, msg, spinnerStyle := parseFlags()
+	ips, interval, msg, spinnerStyle := parseFlags()
 	spinner := getSpinnerStyle(spinnerStyle)
 
-	i := 0
+	fmt.Print("\033[2J\033[?25l")
+	defer fmt.Print("\033[?25h")
+
+	status := make([]bool, len(ips))
+	spinnerIndex := 0
+
+	go func() {
+		for {
+			spinnerIndex = (spinnerIndex + 1) % len(spinner)
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	for i, ip := range ips {
+		go func(i int, ip string) {
+			for {
+				status[i] = checkConnection(ip, msg)
+				time.Sleep(time.Duration(interval) * time.Millisecond)
+			}
+		}(i, ip)
+	}
 
 	for {
-		if checkConnection(ip, msg) {
-			printStatus(true, spinner, i, ip)
-		} else {
-			printStatus(false, spinner, i, ip)
+		for i, ip := range ips {
+			printStatus(status[i], spinner, spinnerIndex, ip, i)
 		}
-
-		i = (i + 1) % len(spinner)
-		time.Sleep(time.Duration(interval) * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 	}
 }
